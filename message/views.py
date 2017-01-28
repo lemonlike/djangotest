d9dfeabcc99c71a4c145a7555520e0c3a5aca1f7
Lemon <962650082@qq.com>
2017-01-28 15:08:03 +0800

    Modify files

--- message/views.py
+++ message/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
-
+from .models import UserMessage
 # Create your views here.
 
 
 def getform(request):
+    if request.method == 'POST':
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        address = request.POST.get('address', '')
+        message = request.POST.get('message', '')
+
+        user_message = UserMessage()
+        user_message.name = name
+        user_message.email = email
+        user_message.address = address
+        user_message.message = message
+        user_message.save()
     return render(request, 'message_form.html')
